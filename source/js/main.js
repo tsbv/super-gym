c71ad2b997d3864b6d01ab154e0dd69c3f3fb5c1
Georgij Tsabiev <gtsabiev@inbox.ru>
2024-07-03 13:54:33 +0300

    Стилизует секцию about мобильной версии с блоком видео

--- source/js/main.js
+++ source/js/main.js
@@ -2,3 +2,14 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
+const video = document.querySelector('.video');
+video.addEventListener('click', () => {
+  if (video.classList.contains('ready')) {
+    return;
+  }
+  video.classList.add('ready');
+
+  const src = video.dataset.src;
+
+  video.insertAdjacentHTML('afterbegin', `<iframe class="video__iframe" src="${src}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`);
+});
