1275e4a4910209981012bb4e57dfde3376150fbc
Georgyi Tsabiev <gtsabiev@inbox.ru>
2024-07-03 16:12:18 +0300

    Рефакторит js код

--- source/js/main.js
+++ source/js/main.js
@@ -3,13 +3,11 @@
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
 const video = document.querySelector('.video');
+const src = video.dataset.src;
 video.addEventListener('click', () => {
-  if (video.classList.contains('ready')) {
+  if (video.classList.contains('video--loaded')) {
     return;
   }
-  video.classList.add('ready');
-
-  const src = video.dataset.src;
-
+  video.classList.add('video--loaded');
   video.insertAdjacentHTML('afterbegin', `<iframe class="video__iframe" src="${src}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`);
 });
