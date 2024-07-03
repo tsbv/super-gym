562ec7fe639812b53c9c710dc65b8862e261c9c2
Georgyi Tsabiev <gtsabiev@inbox.ru>
2024-07-03 18:53:10 +0300

    Стилизует секцию price с визуальным эффектом тени цены

--- source/js/main.js
+++ source/js/main.js
@@ -11,3 +11,10 @@ video.addEventListener('click', () => {
   video.classList.add('video--loaded');
   video.insertAdjacentHTML('afterbegin', `<iframe class="video__iframe" src="${src}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`);
 });
+
+/* Тень цены */
+const priceElements = document.querySelectorAll('.gym-membership__price');
+priceElements.forEach((priceElement) => {
+  const price = priceElement.textContent.trim();
+  priceElement.style.setProperty('--price-shadow', `"${price}"`);
+});
