11b6a4f057f873d4cc739c9b2914ed679374b356
Georgij Tsabiev <gtsabiev@inbox.ru>
2024-07-17 22:57:34 +0300

    Реализует работу аккордеонов

--- source/js/main.js
+++ source/js/main.js
@@ -59,3 +59,15 @@ const swiper1 = new Swiper(slider1, {
     prevEl: '.swiper-button-prev',
   },
 });
+
+/* Аккордеоны */
+const items = document.querySelectorAll('.accordion button');
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+  if (itemToggle === 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  } else {
+    this.setAttribute('aria-expanded', 'false');
+  }
+}
+items.forEach((item) => item.addEventListener('click', toggleAccordion));
