1bbc02e3be4b1b096c92b37db44d98c64d43c24a
Georgij Tsabiev <gtsabiev@inbox.ru>
2024-07-18 23:55:07 +0300

    Реализует работу табов в секции абонементов

--- source/js/main.js
+++ source/js/main.js
@@ -71,3 +71,28 @@ function toggleAccordion() {
   }
 }
 items.forEach((item) => item.addEventListener('click', toggleAccordion));
+
+/* Табы с ценами абонементов */
+const priceListButtons = document.querySelectorAll('.price-list__button');
+const gymMembershipPrices = document.querySelectorAll('.gym-membership__price-text');
+const gymMembershipPriceElements = document.querySelectorAll('.gym-membership__price');
+const priceData = {
+  '1 месяц': ['5000', '1700', '2700'],
+  '6 месяцев': ['30000', '10200', '16200'],
+  '12 месяцев': ['60000', '20400', '32400']
+};
+const updatePrices = (period) => {
+  const prices = priceData[period];
+  gymMembershipPrices.forEach((price, index) => {
+    price.textContent = prices[index];
+    gymMembershipPriceElements[index].style.setProperty('--price-shadow', `"${prices[index]}"`);
+  });
+};
+priceListButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    priceListButtons.forEach((btn) => btn.classList.remove('price-list__button--active'));
+    button.classList.add('price-list__button--active');
+    updatePrices(button.textContent);
+  });
+});
+updatePrices('1 месяц');
