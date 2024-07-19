cd0c498f9cee2cca3508a7607de3cb453eb1c710
Georgij Tsabiev <gtsabiev@inbox.ru>
2024-07-19 23:24:50 +0300

    Реализует валидацию формы

--- source/js/main.js
+++ source/js/main.js
@@ -165,3 +165,23 @@ faqListButtons.forEach((button) => {
     updateFaqContent(button.textContent);
   });
 });
+
+const NAME_REGEX = /^[a-zA-Zа-яА-Я\s]+$/;
+const PHONE_REGEX = /^\d+$/;
+const form = document.querySelector('.form');
+const nameInput = form.querySelector('input[name="name"]');
+const phoneInput = form.querySelector('input[name="phone"]');
+const nameError = form.querySelector('.form__error--name');
+const phoneError = form.querySelector('.form__error--phone');
+const validateInput = (input, regex, error) => {
+  const isValid = regex.test(input.value);
+  error.style.display = isValid ? 'none' : 'block';
+  return isValid;
+};
+form.addEventListener('submit', (event) => {
+  const isNameValid = validateInput(nameInput, NAME_REGEX, nameError);
+  const isPhoneValid = phoneInput.value ? validateInput(phoneInput, PHONE_REGEX, phoneError) : true;
+  if (!isNameValid || !isPhoneValid) {
+    event.preventDefault();
+  }
+});
