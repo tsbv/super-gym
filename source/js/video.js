35219a00b9f1b2eb11126c392bfe9ffd97820fdd
Georgij Tsabiev <gtsabiev@inbox.ru>
2024-07-26 00:47:13 +0300

    Рефакторит реализацию видео в разметке и JS функционал

--- source/js/video.js
+++ source/js/video.js
@@ -1,12 +1,13 @@
 const loadVideo = () => {
   const videoEl = document.querySelector('.video');
-  const src = videoEl.dataset.src;
-  videoEl.addEventListener('click', () => {
+  const playButton = document.querySelector('.video__play-button');
+  const src = `${videoEl.dataset.src}?autoplay=1`;
+  playButton.addEventListener('click', () => {
     if (videoEl.classList.contains('video--loaded')) {
       return;
     }
     videoEl.classList.add('video--loaded');
-    videoEl.insertAdjacentHTML('afterbegin', `<iframe class="video__iframe" src="${src}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`);
+    videoEl.insertAdjacentHTML('afterbegin', `<iframe class="video__iframe" src="${src}" title="Бесплатные интерактивные онлайн-курсы" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`);
   });
 };
 
