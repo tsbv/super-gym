86f3ffcd86e6071fe8777e55b41e7f25314244e4
Georgij Tsabiev <gtsabiev@inbox.ru>
2024-07-26 00:52:15 +0300

    Сокращает названия переменных

--- source/js/main.js
+++ source/js/main.js
@@ -6,7 +6,7 @@ import { initReviewSlider } from './review-slider';
 import { initFaqTabs } from './faq-tabs';
 import { initAccordions } from './accordions';
 import { initFormValidation } from './form-validation';
-import { initGymMembershipHover } from './membership-card-hover';
+import { initMembershipHover } from './membership-card-hover';
 
 loadVideo(); // Загрузка видео
 initPriceTabs(); // Табы с ценами абонементов
@@ -16,4 +16,4 @@ initReviewSlider(); // Слайдер отзывов
 initFaqTabs(); // Табы с аккордеонами
 initAccordions(); // Аккордеоны
 initFormValidation(); // Валидация формы
-initGymMembershipHover(); // Карточка абонемента при наведении на кнопку внутри неё
+initMembershipHover(); // Карточка абонемента при наведении на кнопку внутри неё
--- source/js/membership-card-hover.js
+++ source/js/membership-card-hover.js
@@ -1,14 +1,14 @@
-const initGymMembershipHover = () => {
-  const gymMembershipLinkNodes = document.querySelectorAll('.gym-membership__link');
-  gymMembershipLinkNodes.forEach((gymMembershipLinkNode) => {
-    const gymMembershipEl = gymMembershipLinkNode.closest('.gym-membership');
-    gymMembershipLinkNode.addEventListener('mouseenter', () => {
-      gymMembershipEl.style.background = 'linear-gradient(151deg, #ed0233 0%, #003eb7 100%)';
+const initMembershipHover = () => {
+  const membershipLinkNodes = document.querySelectorAll('.gym-membership__link');
+  membershipLinkNodes.forEach((membershipLinkNode) => {
+    const membershipEl = membershipLinkNode.closest('.gym-membership');
+    membershipLinkNode.addEventListener('mouseenter', () => {
+      membershipEl.style.background = 'linear-gradient(151deg, #ed0233 0%, #003eb7 100%)';
     });
-    gymMembershipLinkNode.addEventListener('mouseleave', () => {
-      gymMembershipEl.style.background = '';
+    membershipLinkNode.addEventListener('mouseleave', () => {
+      membershipEl.style.background = '';
     });
   });
 };
 
-export { initGymMembershipHover };
+export { initMembershipHover };
