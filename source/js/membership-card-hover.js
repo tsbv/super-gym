cefca697a3d090ac86fb977cb963358419a3c2d4
Georgij Tsabiev <gtsabiev@inbox.ru>
2024-07-26 00:16:15 +0300

    Рефакторит JS код реализации состояний карточки абонементов

--- source/js/membership-card-hover.js
+++ source/js/membership-card-hover.js
@@ -1,11 +1,13 @@
 const initGymMembershipHover = () => {
-  const gymMembershipLink = document.querySelector('.gym-membership__link');
-  const gymMembership = gymMembershipLink.closest('.gym-membership');
-  gymMembershipLink.addEventListener('mouseenter', () => {
-    gymMembership.style.background = 'linear-gradient(151deg, #ed0233 0%, #003eb7 100%)';
-  });
-  gymMembershipLink.addEventListener('mouseleave', () => {
-    gymMembership.style.background = '';
+  const gymMembershipLinks = document.querySelectorAll('.gym-membership__link');
+  gymMembershipLinks.forEach((gymMembershipLink) => {
+    const gymMembership = gymMembershipLink.closest('.gym-membership');
+    gymMembershipLink.addEventListener('mouseenter', () => {
+      gymMembership.style.background = 'linear-gradient(151deg, #ed0233 0%, #003eb7 100%)';
+    });
+    gymMembershipLink.addEventListener('mouseleave', () => {
+      gymMembership.style.background = '';
+    });
   });
 };
 
