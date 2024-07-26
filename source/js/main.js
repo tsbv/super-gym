484ea7c3b40338d1c7a454c310370ae4b2939718
Georgij Tsabiev <gtsabiev@inbox.ru>
2024-07-26 13:50:12 +0300

    Реализует состояние карточки абоненемента используя :has()

--- source/js/main.js
+++ source/js/main.js
@@ -6,7 +6,6 @@ import { initReviewSlider } from './review-slider';
 import { initFaqTabs } from './faq-tabs';
 import { initAccordions } from './accordions';
 import { initFormValidation } from './form-validation';
-import { initMembershipHover } from './membership-card-hover';
 
 loadVideo(); // Загрузка видео
 initPriceTabs(); // Табы с ценами абонементов
@@ -16,4 +15,3 @@ initReviewSlider(); // Слайдер отзывов
 initFaqTabs(); // Табы с аккордеонами
 initAccordions(); // Аккордеоны
 initFormValidation(); // Валидация формы
-initMembershipHover(); // Карточка абонемента при наведении на кнопку внутри неё
